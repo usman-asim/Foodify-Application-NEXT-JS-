eff2ae6cc82a07a24aaccd9d401bd6f47d1d508b
Usman Asim <usmandevlpr@gmail.com>
2026-01-23 00:44:45 +0500

    Update route.ts

--- frontened/app/api/create-payment-intent/route.ts
+++ frontened/app/api/create-payment-intent/route.ts
@@ -2,15 +2,17 @@ import Stripe from "stripe";
 import { NextRequest } from "next/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-12-15.clover",
+  apiVersion: "2022-11-15",
 });
 
 export async function POST(req: NextRequest) {
   const { amount } = await req.json();
 
+  console.log("Amount received in backend (in cents):", amount);
+
   try {
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount, 
+      amount: amount, // Already in cents
       currency: "usd",
       automatic_payment_methods: { enabled: true },
     });
@@ -24,7 +26,8 @@ export async function POST(req: NextRequest) {
       },
     );
   } catch (error) {
-    const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
+    const errorMessage =
+      error instanceof Error ? error.message : "An unknown error occurred";
     return new Response(JSON.stringify({ error: errorMessage }), {
       status: 500,
       headers: {
