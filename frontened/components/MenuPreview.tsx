5c8a1d6c8397c5565f77924cce9257f31b27c1d0
Usman Asim <usmandevlpr@gmail.com>
2026-01-23 23:39:34 +0500

    Update MenuPreview.tsx

--- frontened/components/MenuPreview.tsx
+++ frontened/components/MenuPreview.tsx
@@ -28,7 +28,7 @@ export default async function MenuPreview() {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-5">
         {menuItems.map((item, i) => (
            <div
             key={item.id}
